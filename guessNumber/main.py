5de7cdfd56eb97e35320b69c2f41de00a9a14ad6
lsimonidev <laura.simoni@gmail.com>
2022-07-23 06:33:36 +0100

    add computer_guess where the computer has to guess our secret number

--- guessNumber/main.py
+++ guessNumber/main.py
@@ -11,4 +11,25 @@ def guess(x):
             print("Too low!")
     print(f"You've guessed the number {random_number} correctly!")
     
-guess(10)
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if high != low:
+            guess = random.randint(low, high)
+        else:
+            guess = high
+
+        feedback = input(f"Is {guess} too high(H), too low(L) or correct(C)? ").lower()
+
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+        print(low,high)
+    print(f"I've guessed the number {guess} correctly")
+
+
+
+computer_guess(10)
